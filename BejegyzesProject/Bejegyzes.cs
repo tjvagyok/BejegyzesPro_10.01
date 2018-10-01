2e0b037ccde5e559c5c1f57abb337ae0aa7f9e1a
bartab9610 <barnabas96@freemail.hu>
2018-10-01 15:59:04 +0200

    Kesz a program

---
+++ BejegyzesProject/Bejegyzes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProject
+{
+    class Bejegyzes
+    {
+        String szerzo;
+        String tartalom;
+        int likeok = 0;
+        DateTime letrejott = new DateTime(2018, 10, 01);
+        DateTime szerkesztve = DateTime.Now;
+
+
+        public Bejegyzes(String szerzo,String tartalom)
+        {
+
+        }
+
+        
+  
+
+    public void Like ()
+        {
+            likeok++;
+
+            return likeok;
+        }
+                    
+        function  ()
+        {
+
+        }
+        List<>
+
+    }
+    
+
+
+}
+
